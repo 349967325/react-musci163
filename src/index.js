e2f613375f714c13bfa4e6841222240fb37d4066
龙升 <longsheng@iiasaas.com>
2020-04-23 15:58:17 +0800

    fix:暂存

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // import "babel-polyfill"
 import 'core-js/es'
-import 'react-app-polyfill/ie11'  
+import 'react-app-polyfill/ie9'  
 import 'react-app-polyfill/stable'
 
 import React from 'react';
