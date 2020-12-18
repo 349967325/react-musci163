a4b66be8a79d36e6de04333fbf7443d3edf4c689
龙升 <longsheng@iiasaas.com>
2020-12-18 14:06:54 +0800

    fix:暂存

--- src/componetns/global-footer/index.jsx
+++ src/componetns/global-footer/index.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
 
+import styles from './footer.module.scss'
+
 class GlobalFooter extends Component {
   render() {
     return (
-      <div />
+      <div className={`${styles.footerBar}`}>
+        这是页面底部
+      </div>
     )
   }
 }
